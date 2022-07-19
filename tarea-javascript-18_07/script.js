e84941fb4c239fef96c94cd5251ab7e7c66ab20b
javierhuebra <javieremanuelhuebra@gmail.com>
2022-07-19 00:14:28 -0300

    :bug: Feat: upload first code js

---
+++ tarea-javascript-18_07/script.js
@@ -0,0 +1,16 @@
+let name = "Javier Emanuel";
+let age = 29;
+let isSingle = true;
+let array = [1,2,3,4,5,6,7,8,9,10];
+
+let skillers = ["Abel Hernan Reyes","Adán Rodriguez","Adrian Fornaso","Alan Vilate","Alejandro Toloza"];
+
+let objeto = {
+    name: "Javier Emanuel",
+    age: 29,
+    isSingle: true,
+    array: [1,2,3,4,5,6,7,8,9,10],
+    skillers: ["Abel Hernan Reyes","Adán Rodriguez","Adrian Fornaso","Alan Vilate","Alejandro Toloza"]
+}
+
+console.log(objeto);
